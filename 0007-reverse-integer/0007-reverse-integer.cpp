5d6d98165c2bfbaeb98369ce0e818d83cc796c18
Aditya Gupta <aditya.gupta.0910@gmail.com>
2026-06-26 00:13:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.42%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        long long int rev = 0;
+        
+        for (; x != 0; x = x / 10) {
+            rev = rev * 10 + x % 10;
+        }
+        
+        if ((rev < (-1) * pow(2,31)) || (rev > pow(2,31) - 1))
+            return 0;
+        else
+            return (int)rev;
+    }
+};
