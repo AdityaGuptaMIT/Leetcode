955476ec106ebb15294eb13678c3f2481de37cee
Aditya Gupta <aditya.gupta.0910@gmail.com>
2026-08-09 14:52:20 +0530

    Time: 963 ms (6.11%), Space: 30.3 MB (38.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -9,9 +9,7 @@ public:
         for (int i=0 ; i<nums.size()-1 ; i++) {
             for (int j=0 ; j<nums.size()-i-1 ; j++) {
                 if (nums[j] > nums[j+1]) {
-                    int temp = nums[j] ;
-                    nums[j] = nums[j+1] ;
-                    nums[j+1] = temp ;
+                    swap (nums[j], nums[j+1]) ;
                 }
             }
         }
