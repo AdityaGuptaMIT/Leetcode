bf34f912e940abf8d0d08bf1fd5bef7af84f106c
Aditya Gupta <aditya.gupta.0910@gmail.com>
2026-08-14 21:30:22 +0530

    Time: 0 ms (100%), Space: 8.2 MB (94.79%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -3,9 +3,8 @@ public:
     int rearrangeCharacters(string s, string target) {
         
         int count = 0 ;
-        bool found = true ;
 
-        while (found != false) {
+        while (true) {
 
             for (int i=0 ; i<target.size() ; i++) {
                 if (s.find(target[i]) != string::npos) {
@@ -15,7 +14,6 @@ public:
                     return count ;
                 }
             }
-            found = true ;
             count++ ;
         }
     
