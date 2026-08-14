99938330543f78293782b9d2cd0fbd406d730536
Aditya Gupta <aditya.gupta.0910@gmail.com>
2026-08-14 21:14:15 +0530

    Time: 2 ms (58.73%), Space: 11.6 MB (98.28%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
 
-        bool found = false ;
-
         for (int i=0 ; i<ransomNote.size() ; i++) {
             if (magazine.find(ransomNote[i]) != string::npos) {
-                found = true ;
                 magazine.erase(magazine.begin() + magazine.find(ransomNote[i])) ;
             }
             else {
-                found = false ;
-                break ;
+                return false ;
             }
         }
 
-        return found ;
+        return true ;
     }
 };
